9ccd57d8aee3f574f55964e892d2eccf8da839a1
Ilya <hairais@mail.ru>
2021-10-08 00:22:59 +0700

    v1.3

--- Main.py
+++ Main.py
@@ -4,44 +4,49 @@ app = Client("my_account")
 commands = Commands(app)
 
 
-@app.on_message(filters.text & filters.private & filters.command("help"))
+@app.on_message(filters.text & filters.command("help"))
 async def ShowHelpCommand(_, message: filters.Message):
     return await commands.ShowHelp(message)
 
 
-@app.on_message(filters.text & filters.private & filters.command("work"))
+@app.on_message(filters.text & filters.command("work"))
 async def ShowDutiesCommand(_, message: filters.Message):
     return await commands.ShowDuties(message)
 
 
-@app.on_message(filters.text & filters.private & filters.command("timetable"))
+@app.on_message(filters.text & filters.command("timetable"))
 async def ShowTimetableCommand(_, message: filters.Message):
     return await commands.ShowTimetable(message)
 
 
-@app.on_message(filters.text & filters.private & filters.command("time"))
+@app.on_message(filters.text & filters.command("time"))
 async def ShowTimeCommand(_, message: filters.Message):
     return await commands.ShowTime(message)
 
 
-@app.on_message(filters.text & filters.private & filters.command("hw"))
+@app.on_message(filters.text & filters.command("hw"))
 async def ShowHomeworkCommand(_, message: filters.Message):
     return await commands.ShowHomework(message)
 
 
-@app.on_message(filters.private & filters.command("black"))
+@app.on_message(filters.command("black"))
 async def ShowBlackCommand(_, message: filters.Message):
     return await commands.ShowBlack(message)
 
 
-@app.on_message(filters.text & filters.private & filters.command("newhw"))
+@app.on_message(filters.text & filters.command("newhw"))
 async def CreateHomeworkCommand(_, message: filters.Message):
     return await commands.CreateHomework(message)
 
 
-@app.on_message(filters.text & filters.private & filters.command("echo"))
+@app.on_message(filters.text & filters.command("echo"))
 async def EchoCommand(_, message: filters.Message):
     return await commands.Echo(message)
+
+
+@app.on_message(filters.text & filters.command("repeat"))
+async def RepeatCommand(_, message: filters.Message):
+    return await commands.Repeat(message)
 
 
 def Main():
